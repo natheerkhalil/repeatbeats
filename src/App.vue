7fd756031151b509be2191549edc5acc5f3d4130
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-05 21:22:44 +0100

    Added template footer element to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <router-view v-slot="{ Component }">
+  <div style="flex: 1;" class="_flex _fd-co">
+    <router-view v-slot="{ Component }">
       <component :is="Component" />
-  </router-view>
-  <ResponseMsg></ResponseMsg>
+    </router-view>
+    <ResponseMsg></ResponseMsg>
+  </div>
+  <Footer></Footer>
 </template>
 
 <script>
 import { uauth } from "@/utils/auth";
 
 import ResponseMsg from "@/components/ResponseMsg.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     ResponseMsg,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="__b _flex _cc __bg-grey-4">
+        <div class="__13 __w __mauto _flex __padxs">
+            <p class="__txt-grey-10">Footer</p>
+        </div>
+    </div>
+</template>
