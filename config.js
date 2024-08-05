5266e5ddd78c75870c4fbcf84e6414fb3a851488
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-05 13:23:24 +0100

    Configure config.js to use environment variables

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://xylophone-api-d07fa4529327.herokuapp.com/api";
-//export const YT_API_KEY = process.env.YT_API_KEY;
+export const API_URL = import.meta.env.VITE_API_URL;
+export const YT_API_KEY = import.meta.env.VITE_YT_API_KEY;
