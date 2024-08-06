03714cde41d7a0dd9fcd3e387bc50e30d6433957
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-06 13:34:44 +0100

    Added T&C and Privacy Policy

---
+++ src/views/Privacy.vue
@@ -0,0 +1,131 @@
+<template>
+    <div style="border-bottom: 1px solid var(--grey-3); box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);"
+        class="__b __po __bg-grey-2 _flex _ai-ce _fd-ro _jc-be __padsm">
+        <div @click="goHome" class="_flex _cc _fd-ro">
+            <img style="width: 35px;" src="/icon.png" alt="Logo"> &nbsp;
+            &nbsp;
+            <p class="logo-text __tmd __txt-grey-10">RepeatBeats</p> &nbsp;
+        </div>
+    </div>
+
+    <br><br>
+
+    <div class="_flex __10 __w __mauto _fd-co __padsm">
+
+        <h1>Privacy Policy</h1>
+
+        <br>
+
+        <p class="__mauto __tal __tme __txt-grey-3">Last updated: 6th August 2024</p>
+
+        <br>
+
+        <p>This Privacy Policy outlines how we collect, use, share, and protect your information when you use our
+            website ("Site") and services ("Services"). By accessing or using our Site, you agree to the terms of this
+            Privacy Policy. If you do not agree, please do not use our Site or Services.</p>
+
+        <br>
+
+        <h1>1. Data Collection</h1>
+        <p>RepeatBeats collects the following user data: <br>
+
+            Personal Information: <br> Email address, username, and password. <br>
+            Our databases do not store any other personal data. Please refer to the privacy policies of our third-party
+            services for information on the data they collect.</p>
+
+        <br><br>
+
+        <h1>2. Data Usage</h1>
+        <p>The data we collect is used solely for: <br>
+
+            - Authenticating users. <br>
+            - Providing our services to users. <br>
+            We do not use your data for any other purposes.</p>
+
+        <br><br>
+
+        <h1>3. Data Sharing</h1>
+        <p>RepeatBeats does not sell or share your data with third parties, except where necessary to provide our
+            services. For instance:
+            <br>
+            Mailtrap: To send account-related emails (e.g., forgot password, change email). <br>
+            Stripe: To process payments securely. <br>
+            We may also share your data if required by law or to protect our rights and safety.
+        </p>
+
+        <br><br>
+
+        <h1>4. User Rights</h1>
+        <p>Users have the right to delete any data they have provided to RepeatBeats by deleting their account. This will delete all of the user's personal data such as their
+            Email, Username, Password, as well as all content created and shared by the user, such as Videos and Playlists.
+            Be aware that some data may still be stored by third-party services for legal or administrative reasons. To view, update, and/or delete account information, visit <a href="https://repeatbeats.com/account" target="_blank">https://repeatbeats.com/account</a></p>
+
+        <br><br>
+
+        <h1>5. Data Security</h1>
+        <p>We are committed to protecting your data:
+
+            All data is transmitted over a secure HTTPS connection. <br>
+            Our website is protected with an HTTPS certificate. <br>
+            Only essential personal data is stored in our databases, though it may also be stored by third-party services when required.</p>
+
+        <br><br>
+
+        <h1>6. Contact Information</h1>
+
+        <p>RepeatBeats uses cookies strictly necessary for the functionality of the Site:
+
+            We do not use any tracking cookies or third-party analytics tools.
+            Non-personal data may be cached to improve performance, which users can refresh by pressing the "sync data"
+            button or by manually clearing local storage.
+            Please refer to the privacy policies of our third-party services for information on their use of cookies and
+            tracking technologies. </p>
+
+        <br><br>
+
+        <h1>7. International Data Transfers</h1>
+        <p>If your data is transferred outside your country, we ensure it is protected in accordance with this Privacy
+            Policy and applicable laws.</p>
+
+        <br><br>
+
+        <h1>8. Changes to the Privacy Policy</h1>
+        <p>We may update this Privacy Policy from time to time. Users will be notified of any changes via email, where
+            possible, or through a notification on the website. By continuing to use the Site after changes become
+            effective, you agree to be bound by the revised Privacy Policy.</p>
+
+        <br><br>
+
+        <h1>9. Contact</h1>
+        <p>If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at <a href="mailto:privacy@repeatbeats">privacy@repeatbeats</a>.</p>
+
+        <br><br>
+
+        <p>By using RepeatBeats, you acknowledge that you have read, understood, and agree to be bound by this Privacy
+        Policy.</p>
+
+        <br><br><br>
+    </div>
+</template>
+
+<style scoped>
+div p {
+    text-align: justify;
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+
+    methods: {
+        goHome() {
+            this.$router.push('/')
+        }
+    }
+
+}
+</script>
---
+++ src/views/Terms.vue
@@ -0,0 +1,240 @@
+<template>
+    <div style="border-bottom: 1px solid var(--grey-3); box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);"
+        class="__b __po __bg-grey-2 _flex _ai-ce _fd-ro _jc-be __padsm">
+        <div @click="goHome" class="_flex _cc _fd-ro">
+            <img style="width: 35px;" src="/icon.png" alt="Logo"> &nbsp;
+            &nbsp;
+            <p class="logo-text __tmd __txt-grey-10">RepeatBeats</p> &nbsp;
+        </div>
+    </div>
+
+    <br><br>
+
+    <div class="_flex __10 __w __mauto _fd-co __padsm">
+        <h1>Terms & Conditions</h1>
+
+        <br>
+
+        <p class="__mauto __tal __tme __txt-grey-3">Last updated: 6th August 2024</p>
+
+        <br>
+
+        <p>These Terms and Conditions ("T&C") govern your use of the RepeatBeats website ("Site") and services
+            ("Services"). By
+            accessing or using our Site, you agree to comply with and be bound by these T&C. If you do not agree to
+            these
+            T&C,
+            please do not use our Site or Services.</p>
+
+        <br><br>
+
+        <h1>Definitions</h1>
+
+        <p><strong>Service</strong>: The functionalities provided by RepeatBeats. <br>
+            <strong>Content</strong>: Any information, videos, playlists, or other material uploaded or linked by users.
+            <br>
+            <strong>User</strong>: Anyone who accesses or registers to use RepeatBeats. <br>
+            <strong>We/Our/Us</strong>: Refers to RepeatBeats and its operators. <br>
+            <strong>You/Your</strong>: Refers to the user of the Service.
+        </p>
+
+        <br><br>
+
+        <h1>1. Jurisdiction and Governing Law</h1>
+        <p>These T&C are governed by and construed in accordance with the laws of the United Kingdom. Any disputes
+            arising
+            out
+            of or in connection with these T&C will be subject to the exclusive jurisdiction of the courts of England
+            and
+            Wales.</p>
+
+        <br><br>
+
+        <h1>2. Age Requirement</h1>
+        <p>While RepeatBeats does not contain explicitly inappropriate content, users must adhere to any applicable laws
+            in
+            their jurisdiction regarding age restrictions. Users under the age of 13 must have parental consent to
+            register
+            for
+            an account and must provide a parent's email address for verification.</p>
+
+        <br><br>
+
+        <h1>3. Account Registration</h1>
+        <p>To use our Services, you must register for an account by providing a username, email address, and password.
+            You
+            are
+            responsible for maintaining the confidentiality of your account information and for all activities that
+            occur
+            under
+            your account.</p>
+
+        <br><br>
+
+        <h1>4. Account Permissions</h1>
+        <p>Users with an unverified email address can create up to 10 videos and 1 playlist. Verifying
+            your
+            email
+            increases this limit to 100 videos and 10 playlists. You can further upgrade to unlimited storage by paying
+            a
+            one-time fee. Note that users that abuse this "unlimited storage" with malicious intent including, but not
+            limited to: <br><br>
+
+            - Overwhelming the server with requests <br>
+            - Deliberately incurring extra costs <br>
+            - Storing prohibited content (e.g., illegal, offensive, or copyrighted material without permission) <br>
+            - Engaging in activities that violate our terms of service <br>
+            - Utilizing the storage for hosting large amounts of unnecessary or redundant data <br>
+            - Attempting to exploit system vulnerabilities <br>
+            - Engaging in activities that degrade system performance or affect other users <br><br>
+            may face account suspension or termination of service. Our system monitors for such abuses, and appropriate
+            measures will be taken to ensure the integrity and availability of our services. Such users will not receive a refund if having made a payment.
+            Decisions regarding abuse
+            and suspensions are left to our discretion.
+        </p>
+
+        <br><br>
+
+        <h1>5. User Conduct</h1>
+        <p>Users are expected to use RepeatBeats responsibly. Any abuse of the "share video" feature, such as using it
+            to
+            harass other users, or abuse of any other feature provided
+            by the Service, may result in restricted access. RepeatBeats reserves the right to take
+            appropriate action, including restricting or terminating your account.</p>
+
+        <br><br>
+
+        <h1>6. Content Ownership</h1>
+        <p>RepeatBeats claims no ownership over the content you create, including YouTube videos, modifications to
+            videos,
+            or
+            playlists. By using our Service, you grant RepeatBeats a license to use your content for the purpose of
+            providing
+            the service. Users retain full ownership of their content but must respect the copyrights of third-party
+            content,
+            such as YouTube videos. RepeatBeats is not liable for any copyright infringements by users.</p>
+
+        <br><br>
+
+        <h1>7. Modification of Services</h1>
+        <p>RepeatBeats reserves the right to modify or discontinue the Service at any time without prior notice. Where
+            feasible, we will provide reasonable notice before making significant changes.</p>
+
+        <br><br>
+
+        <h1>8. Limitation of Liability</h1>
+        <p>To the maximum extent permitted by law, RepeatBeats will not be liable for any indirect, incidental, special,
+            consequential, or punitive damages, or any loss of profits or revenues, whether incurred directly or
+            indirectly,
+            or
+            any loss of data, use, goodwill, or other intangible losses, resulting from your use of the Site and
+            Services.</p>
+
+        <br><br>
+
+        <h1>9. Intellectual Property</h1>
+        <p>RepeatBeats owns all rights to the software and branding used on the Site. You may not use any of our
+            trademarks,
+            service marks, or copyrighted materials without our prior written consent. The content typically generated
+            by
+            users,
+            such as YouTube videos and their modifications, are usually the property of third parties unless otherwise
+            stated.
+            Users must respect these rights.</p>
+
+        <br><br>
+
+        <h1>10. Third-Party Services</h1>
+        <p>RepeatBeats uses third-party services, such as (but not limited to) the Genius API (for lyrics), the YouTube
+            IFrame API (for video
+            playback), Stripe (for payment processing), Mailtrap (for sending account emails). These third parties may
+            collect data as per their own
+            privacy
+            policies. RepeatBeats does not sell or store your personal data but uses cookies for necessary functions
+            such as
+            authentication. Please review the privacy policies of these third-party services for more information.</p>
+
+        <br><br>
+
+        <h1>11. Payment and Refund Policy</h1>
+        <p>To upgrade your storage to unlimited videos and playlists, a one-time fee of £4.99 is required. This fee is
+            non-refundable. All payment transactions are processed securely through Stripe. RepeatBeats does not store
+            credit
+            card information. By making a payment, you agree to Stripe's privacy policy and terms of service, which you
+            can view here: <a target="_blank" href="https://stripe.com/gb/privacy">https://stripe.com/gb/privacy</a>.
+        </p>
+
+        <br><br>
+
+        <h1>12. Local Storage</h1>
+        <p>Non-personal data such as videos and playlists are cached and stored in your local storage and can be cleared
+            at
+            any
+            time by clicking the "sync data" button.</p>
+
+        <br><br>
+
+        <h1>13. Personal Data</h1>
+        <p> Personal data is not shared with third parties except for necessary
+            purposes (e.g., Stripe for payments, Mailtrap for sending account-related emails). You may delete your
+            account
+            at
+            any time via the account page, which will remove all personal data, as well as all data created by you that
+            is stored by us, including but not limited to:
+            videos, playlists, and shares with other
+            users.
+            However, some data may still be stored by third parties for legal or administrative reasons.</p>
+
+        <br><br>
+
+        <h1>14. Termination of Service</h1>
+
+        <p>RepeatBeats reserves the right to terminate your account and access to the Service at our discretion,
+            including but not limited to abuse of the service, violation of these T&C, or as required by law.</p>
+
+        <br><br>
+
+        <h1>15. Changes to T&C</h1>
+        <p>We may update these T&C from time to time. Users will be notified of any changes via email, where possible,
+            or
+            through a notification on the website. By continuing to use the Site after changes become effective, you
+            agree
+            to be
+            bound by the revised T&C.</p>
+
+        <br><br>
+
+        <h1>16. Contact</h1>
+        <p>For any questions or concerns, please use the "Contact" link in the footer to email us. Alternatively, send
+            us an email
+            at <a href="mailto:info@repeatbeats.com">info@repeatbeats.com</a>.</p>
+
+        <br><br>
+
+        <p>By using RepeatBeats, you acknowledge that you have read, understood, and agree to be bound by these T&C.</p>
+
+        <br><br><br>
+    </div>
+</template>
+
+<style scoped>
+div p {
+    text-align: justify;
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+
+    methods: {
+        goHome() {
+            this.$router.push('/')
+        }
+    }
+
+}
+</script>
