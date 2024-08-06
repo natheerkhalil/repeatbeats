9742e612701483f7e5334178d8a6c2163cef2924
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-06 14:04:13 +0100

    Removed border around footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="__b _flex __bg-grey-4">
-        <div class="__b __bo-flag-1 _fw-wr __bod __w _ai-ce __mauto _flex __padxs">
+        <div class="__b _fw-wr __w _ai-ce __mauto _flex __padxs">
             <p class="__txt-grey-10">&copy; RepeatBeats {{ thisYear }}</p> &nbsp; &nbsp;
             <router-link class="__txt-grey-10 __hv __ht-warn-8" to="/terms">Terms & Conditions</router-link> &nbsp; &nbsp;
             <router-link class="__txt-grey-10 __hv __ht-warn-8" to="/privacy">Privacy Policy</router-link> &nbsp; &nbsp;
