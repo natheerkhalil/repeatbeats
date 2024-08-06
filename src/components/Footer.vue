ed0744732f66132a8c4db50a40460c50cad53806
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-06 13:33:24 +0100

    Updated footer with links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,21 @@
 <template>
-    <div class="__b _flex _cc __bg-grey-4">
-        <div class="__13 __w __mauto _flex __padxs">
-            <p class="__txt-grey-10">Footer</p>
+    <div class="__b _flex __bg-grey-4">
+        <div class="__b __bo-flag-1 _fw-wr __bod __w _ai-ce __mauto _flex __padxs">
+            <p class="__txt-grey-10">&copy; RepeatBeats {{ thisYear }}</p> &nbsp; &nbsp;
+            <router-link class="__txt-grey-10 __hv __ht-warn-8" to="/terms">Terms & Conditions</router-link> &nbsp; &nbsp;
+            <router-link class="__txt-grey-10 __hv __ht-warn-8" to="/privacy">Privacy Policy</router-link> &nbsp; &nbsp;
+            <a target="_blank" href="https://github.com/natheerkhalil/repeatbeats" class="__un __po __txt-grey-10 __hv __ht-warn-8" to="/">View on GitHub</a> &nbsp; &nbsp;
+            <a class="__txt-grey-10 __hv __ht-warn-8" href="mailto:info@repeatbeats.com">Contact</a>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            thisYear: new Date().getFullYear()
+        }
+    }
+}
+</script>
