a8661d9c62202afb2fa1f8ab2d92086361a0a0cd
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-07 12:06:47 +0100

    Add margin top to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="__b _flex __bg-grey-4">
+    <div style="margin-top: 10px;" class="__b _flex __bg-grey-4">
         <div class="__b _fw-wr __w _ai-ce __mauto _flex __padxs">
             <p class="__txt-grey-10">&copy; RepeatBeats {{ thisYear }}</p> &nbsp; &nbsp;
             <router-link class="__txt-grey-10 __hv __ht-warn-8" to="/terms">Terms & Conditions</router-link> &nbsp; &nbsp;
