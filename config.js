c177a5f65d71bf42a536441535ecfad1d2835078
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-12 16:57:15 +0100

    Update with turnstile site key variable

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ export const YT_DATA_KEY = import.meta.env.VITE_YT_DATA_KEY;
 export const MAINTENANCE_MODE = import.meta.env.VITE_MAINTENANCE_MODE;
 export const STRIPE_KEY = import.meta.env.VITE_STRIPE_KEY;
 export const HCAPTCHA_KEY = import.meta.env.VITE_HCAPTCHA_KEY;
+export const TURNSTILE_SITE_KEY = import.meta.env.VITE_TURNSTILE_SITE_KEY
