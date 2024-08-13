3ab61ac504aa99cf11d15b1245ee2b38c2576b83
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-13 12:03:23 +0100

    Add visual feedback when updating playlist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -876,7 +876,7 @@
                 <path
                   d="m4.481 15.659c-1.334 3.916-1.48 4.232-1.48 4.587 0 .528.46.749.749.749.352 0 .668-.137 4.574-1.492zm1.06-1.061 3.846 3.846 11.321-11.311c.195-.195.293-.45.293-.707 0-.255-.098-.51-.293-.706-.692-.691-1.742-1.74-2.435-2.432-.195-.195-.451-.293-.707-.293-.254 0-.51.098-.706.293z"
                   fill-rule="nonzero" />
-              </svg>
+              </svg> <span v-if="pl.isEditable"> &nbsp; &nbsp; </span>
 
               <!-- SAVE & CANCEL EDIT -->
               <svg @click="updatePlaylist(pl.id)" style="margin-right: 7px" class="__po" v-if="pl.isEditable"
@@ -2411,6 +2411,14 @@ export default {
     updatePlaylist(id) {
       let el = document.getElementById('pl_' + id + '_title');
       let name = el.textContent;
+
+      setTimeout(() => {
+        // get playlist element from playlists array
+        let pl = this.playlists.find(p => p.id === id);
+        
+        // set isEditable to false
+        pl["isEditable"] = false;
+      }, 200);
 
       request({ id: id, name: name }, '/playlist/update').then(res => {
         if (!res.failed) {
