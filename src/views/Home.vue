fc2b877cf1090b97c9aaf148cedfba8eeea8f9fe
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-13 17:07:50 +0100

    Remove redundant membership refresh button from Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -68,21 +68,11 @@
             add up to <strong>100</strong> videos and <strong>10</strong> playlists. Upgrade to unlock unlimited
             storage.
           </p>
-          <p style="margin-top: 5px; font-size: 13px;" class="__b __tle __txt-grey-8">Already upgraded on another
-            device? Press the <strong>refresh</strong> button.
-          </p>
         </div>
         <br class="m_hide _sm-show">
         <div style="margin-left: 15px; " class="_flex _cc _fd-ro">
           <button @click="goToUpgrade" style="min-width: max-content;"
             class="__padxs __tsx __bg-none __po __bo-grey-10 __txt-grey-10 __bod">Upgrade Account</button> &nbsp; &nbsp;
-          <svg style="margin-left: 10px; " width=24 height=24 class="__po" fill=var(--grey_10)
-            @click="refreshMembershipStatus" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round"
-            stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path data-v-2dc54a20=""
-              d="m21.897 13.404.008-.057v.002c.024-.178.044-.357.058-.537.024-.302-.189-.811-.749-.811-.391 0-.715.3-.747.69-.018.221-.044.44-.078.656-.645 4.051-4.158 7.153-8.391 7.153-3.037 0-5.704-1.597-7.206-3.995l1.991-.005c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-4.033c-.414 0-.75.336-.75.75v4.049c0 .414.336.75.75.75s.75-.335.75-.75l.003-2.525c1.765 2.836 4.911 4.726 8.495 4.726 5.042 0 9.217-3.741 9.899-8.596zm-19.774-2.974-.009.056v-.002c-.035.233-.063.469-.082.708-.024.302.189.811.749.811.391 0 .715-.3.747-.69.022-.28.058-.556.107-.827.716-3.968 4.189-6.982 8.362-6.982 3.037 0 5.704 1.597 7.206 3.995l-1.991.005c-.414 0-.75.336-.75.75s.336.75.75.75h4.033c.414 0 .75-.336.75-.75v-4.049c0-.414-.336-.75-.75-.75s-.75.335-.75.75l-.003 2.525c-1.765-2.836-4.911-4.726-8.495-4.726-4.984 0-9.12 3.654-9.874 8.426z"
-              fill-rule="nonzero"></path>
-          </svg>
           <svg style="margin-left: 15px; " fill="var(--grey_10)" width=35 height=35 class="__po"
             @click="hideStorageAlert" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round"
             stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -1591,26 +1581,6 @@ export default {
   },
 
   methods: {
-    // REFRESH MEMBERSHIP STATUS
-    refreshMembershipStatus() {
-      request({}, "/account/membership-status").then(res => {
-        if (!res.failed) {
-          if (res.data.data === false || res.data.data === 0) {
-            localStorage.removeItem("user_is_member");
-
-            this.userIsMember = false;
-          } else {
-            localStorage.setItem("user_is_member", 1);
-
-            this.userIsMember = true;
-          }
-
-          this.checkMaxStorage();
-        } else {
-          useResponseStore().updateResponse("Failed to refresh membership status", "err");
-        }
-      });
-    },
     // CHECK MAX STORAGE
     checkMaxStorage() {
       console.log("Checking max storage");
