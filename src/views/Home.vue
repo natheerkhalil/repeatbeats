6ae6cbcdb3768787df9f044b8d2ab9ec6819c50a
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-13 12:49:10 +0100

    Fix color issue with receivedShares svg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -503,7 +503,7 @@
               <!-- received shares -->
               <div class="_flex tooltip">
                 <svg class="__po" @click="this.showSharesModal = !this.showSharesModal"
-                  :fill="this.receivedShares.length > 1 ? 'var(--err_1)' : 'var(--grey_1)'" viewBox="0 0 24 24"
+                  :fill="this.receivedShares.length > 0 ? 'var(--err_1)' : 'var(--info_1)'" viewBox="0 0 24 24"
                   width="28" height="28" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
                   <path
                     d="M24 22.917h-24v-13.275l2-1.456v-7.269h20v7.272l2 1.453v13.275zm-21-10.472v-10.528h18v10.526l-9 5.474-9-5.472zm6-8.916l1.305 2.41 2.695.496-1.888 1.986.36 2.717-2.472-1.183-2.472 1.183.36-2.717-1.888-1.986 2.695-.496 1.305-2.41zm8 6.471v1h-3v-1h3zm2-2v1h-5v-1h5zm0-2v1h-5v-1h5zm0-2v1h-5v-1h5z" />
