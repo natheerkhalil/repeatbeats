8703c609ab0455388032c1e5a85e3aeee8b92fd2
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-23 10:26:00 +0100

    Update styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -147,16 +147,6 @@
               style="transform: rotate(180deg)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M12 12l-12 8v-16l12 8zm0-8v16l12-8-12-8z" />
             </svg>
-            <div class="_flex __mauto _jc-en _ai-ce">
-              <div class="tooltip">
-              <svg fill="var(--grey_3)" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd">
-                <path
-                  d="M3.732 13h1.504s2.32-8.403 2.799-10.263c.156-.605.646-.738.965-.737.319.001.826.224.947.74.581 2.466 3.11 13.908 3.11 13.908s1.597-6.441 1.943-7.891c.101-.425.536-.757 1-.757.464 0 .865.343 1 .707.312.841 1.675 4.287 1.677 4.293h1.591c.346-.598.992-1 1.732-1 1.104 0 2 .896 2 2s-.896 2-2 2c-.741 0-1.388-.404-1.734-1.003-.939-.001-1.856 0-2.266.003-.503.004-.774-.289-.928-.629l-.852-2.128s-1.828 7.367-2.25 8.999c-.153.595-.646.762-.97.758-.324-.004-.847-.198-.976-.783-.549-2.487-2.081-9.369-3.123-14.053 0 0-1.555 5.764-1.936 7.099-.13.454-.431.731-.965.737h-2.268c-.346.598-.992 1-1.732 1-1.104 0-2-.896-2-2s.896-2 2-2c.74 0 1.386.402 1.732 1z" />
-              </svg>
-              <p class="tooltiptext">Video cannot be paused. Recommended for iOS devices</p>
-            </div> &nbsp; 
-              <input type="checkbox" class="custcheck" v-model="musicMode">
-            </div>
             <svg v-if="allVideos.length > 2 && this.loop" @click="nextVideo()" class="__po" width=29 height=29
               xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path d="M12 12l-12 8v-16l12 8zm0-8v16l12-8-12-8z" />
@@ -308,6 +298,20 @@
                 <div class="__b _flex _jc-en _ai-ce">
                   <p class="__txt-grey-5 __tsx">Ignore start & end</p> &nbsp; &nbsp;
                   <input type="checkbox" class="custcheck" v-model="ignoreLimit">
+                </div>
+
+                <br>
+
+                <div class="__b _flex _jc-en _ai-ce">
+                  <div class="tooltip"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                      viewBox="0 0 24 24">
+                      <path
+                        d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1.25 17c0 .69-.559 1.25-1.25 1.25-.689 0-1.25-.56-1.25-1.25s.561-1.25 1.25-1.25c.691 0 1.25.56 1.25 1.25zm1.393-9.998c-.608-.616-1.515-.955-2.551-.955-2.18 0-3.59 1.55-3.59 3.95h2.011c0-1.486.829-2.013 1.538-2.013.634 0 1.307.421 1.364 1.226.062.847-.39 1.277-.962 1.821-1.412 1.343-1.438 1.993-1.432 3.468h2.005c-.013-.664.03-1.203.935-2.178.677-.73 1.519-1.638 1.536-3.022.011-.924-.284-1.719-.854-2.297z" />
+                    </svg>
+                    <p class="tooltiptext">Video cannot be paused</p>
+                  </div> &nbsp; &nbsp;
+                  <p class="__txt-grey-5 __tsx">Music Mode</p> &nbsp; &nbsp;
+                  <input type="checkbox" class="custcheck" v-model="musicMode">
                 </div>
               </div>
 
