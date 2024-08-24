64555cdbf5c3eb0a53b22e6bf0548f2a4878a8b4
Natheer Ahmad <natheerahmad07@gmail.com>
2024-08-24 19:12:22 +0100

    Update footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,6 @@
             <p class="__txt-grey-10">&copy; RepeatBeats {{ thisYear }}</p> &nbsp; &nbsp;
             <router-link class="__txt-grey-10 __hv __ht-warn-8" to="/terms">Terms & Conditions</router-link> &nbsp; &nbsp;
             <router-link class="__txt-grey-10 __hv __ht-warn-8" to="/privacy">Privacy Policy</router-link> &nbsp; &nbsp;
-            <a target="_blank" href="https://github.com/natheerkhalil/repeatbeats" class="__un __po __txt-grey-10 __hv __ht-warn-8" to="/">View on GitHub</a> &nbsp; &nbsp;
             <a class="__txt-grey-10 __hv __ht-warn-8" href="mailto:info@repeatbeats.com">Contact</a> &nbsp; &nbsp;
             <a target="_blank" class="__txt-grey-10 __hv __ht-warn-8" href="https://ko-fi.com/natheer">Donate ☕</a>
         </div>
